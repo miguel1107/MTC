7e6010c64db20d671a6afb75bd13f4be109277d9
Miguel Revilla <mirepg_93@hotmail.com>
2017-02-22 12:01:51 -0500

    autocomplete 100%

--- model/categoria.php
+++ model/categoria.php
@@ -11,7 +11,7 @@ require_once __DIR__.'/../conectar.php';
 
 		public function cargacombo($id){
 			$link=Conectarse();
-			$sql="SELECT c.id_categoria,c.nombre FROM catxtipo ct inner join categoria c on c.id_categoria=ct.id_cat WHERE id_tipo='".$id."' order by id_tipo";
+			$sql="SELECT c.idcategoria,c.nombre FROM catxtipo ct inner join categoria c on c.idcategoria=ct.id_cat WHERE id_tipo='".$id."' order by id_tipo";
 			$rs=pg_query($link,$sql);
 			return $rs;
 		}
